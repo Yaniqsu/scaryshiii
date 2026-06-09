7997215aa5f181b97309213bc486c78c86f12243
Yaniqsu <119526241+Yaniqsu@users.noreply.github.com>
2026-06-09 02:19:06 +0200

    library almost finished

---
+++ Assets/_Game/Code/Scripts/Environment/BooksDisplacer.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using NaughtyAttributes;
+using UnityEngine;
+using Random = System.Random;
+
+public class BooksDisplacer : MonoBehaviour
+{
+    [SerializeField] private GameObject[] books;
+    
+    [Button("Hide Books")]
+    private void DisplaceBooks()
+    {
+        var seed = Mathf.RoundToInt(Mathf.Pow(transform.position.magnitude, 4) % 4 * 100) * gameObject.name.GetHashCode();
+        Debug.Log($"Seed: {seed} {transform.position.magnitude}");
+        var random = new Random(seed);
+
+        var booksToHide = random.Next(0, this.books.Length);
+        var booksList = new List<GameObject>(books);
+
+        for (int i = 0; i < booksToHide; i++)
+        {
+            var index = random.Next(0, booksList.Count - 1);
+            
+            booksList[index].SetActive(false);
+            booksList.RemoveAt(index);
+        }
+        
+        booksList.ForEach(book => book.SetActive(true));
+    }
+    
+    [Button("Hide ALL Books")]
+    private void DisplaceAllBooks()
+    {
+        var allDisplacers = FindObjectsByType<BooksDisplacer>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+
+        foreach (var displacer in allDisplacers)
+        {
+            displacer.DisplaceBooks();
+        }
+    }
+}
